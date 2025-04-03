cc7f089839280c22ce59208e8f344a26d9006f64
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-04-02 21:40:18 -0500

    se implementa la clase event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -12,15 +12,36 @@ class Reminder:
     EMAIL = "email"
     SYSTEM = "system"
     date_time: datetime
-    type: str = EMAIL
+    type: str = field(default=EMAIL)
 
     def __str__(self):
         return f"Reminder on {self.date_time} of type {self.type}"
 
 
-
-
 # TODO: Implement Event class here
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_reminder(self, date_time: datetime, type = Reminder.EMAIL):
+        reminder = Reminder(date_time, type)
+        self.reminders.append(reminder)
+
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self):
+        return f"ID: {self.id}\nEvent title: {self.title}\nDescription: {self.description}\nTime: {self.start_at} - {self.end_at}"
 
 
 # TODO: Implement Day class here
